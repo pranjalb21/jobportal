3a3693ed922613ec455d852569c08706923f0f39
Pranjal Gain <pranjalgain052@gmail.com>
2024-09-22 03:56:43 +0530

    Frontend started. App initialized and cleanup done. Home, Jobs, Dashboard, Login, Register, PostApplication and NotFound page added. Footer and Navbar component added.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import Dashboard from "./pages/Dashboard";
+import PostApplication from "./pages/PostApplication";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/jobs" element={<Jobs />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route
+                        path="/post/application/:jobId"
+                        element={<PostApplication />}
+                    />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </>
+    );
+}
+
+export default App;
